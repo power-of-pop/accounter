ea1474e3170df429a34be74bd68f849e61930b8a
EC2 Default User <ec2-user@ip-172-31-39-216.ap-northeast-1.compute.internal>
2025-04-17 06:51:01 +0000

    表示変更

--- app/controllers/public/transactions_controller.rb
+++ app/controllers/public/transactions_controller.rb
@@ -1,24 +1,37 @@
 class Public::TransactionsController < ApplicationController
+  before_action :set_transaction, only: [:show, :edit]
+
   def new
     @transaction = Transaction.new
   end
 
   def index
+    @transactions = Transaction.all
   end
 
   def show
   end
 
   def create
+    @transaction = Transaction.new(transaction_params)
+    @transaction.user_id = current_user.id
+    if @transaction.save
+      redirect_to transactions_path, notice: '正常に登録されました'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
-  def update
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:amount, :description, :transaction_type, :category_id, :date)
   end
 
-  def destroy
+  def set_transaction
+    @transaction = current_user.transactions.find(params[:id])
   end
-
 end
